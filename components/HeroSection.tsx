6404457c4d5f3ca039ea8eb90f20df9cbe486901
robuGdada <paudelronish@gmail.com>
2024-09-27 20:42:08 +0545

    hero section update

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,16 +3,16 @@ export function HeroSection() {
     <section className="relative bg-green-600 min-h-[60vh] flex items-center">
       <div className="max-w-[1920px] w-full mx-auto px-4 sm:px-6 lg:px-8 z-20 relative">
         <div className="max-w-3xl">
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4 leading-tight font-poppins bg-gradient-to-r from-green-900 to-green-950 bg-clip-text text-transparent dark:bg-gradient-to-r dark:from-white dark:to-gray-300">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4 leading-tight font-poppins bg-gradient-to-r from-gray-100 to-gray-200 bg-clip-text shadow-2xl text-transparent dark:bg-gradient-to-r dark:from-white dark:to-gray-300">
             Eat Healthy, Stay Healthy
           </h1>
-          <p className="text-lg sm:text-xl mb-8 font-poppins text-gray-900 dark:text-gray-200">
+          <p className="text-lg sm:text-xl mb-8 font-poppins bg-gradient-to-r from-gray-100 to-gray-200 text-transparent bg-clip-text dark:text-gray-200 shadow-2xl">
             Discover our range of quality food products for a healthier you. We
             provide quality food products and are involved in agriculture.
           </p>
           <a
             href="/products"
-            className="inline-block text-green-700 bg-white px-6 py-3 rounded-full font-semibold hover:bg-green-800 dark:hover:bg-green-600 transition duration-300 text-sm sm:text-base shadow-lg dark:shadow-md"
+            className="inline-block text-green-700 bg-white px-6 py-3 rounded-full font-semibold hover:bg-green-700 hover:shadow-gray-400 hover:text-gray-100 dark:hover:bg-green-600 transition duration-300 text-sm sm:text-base shadow-lg dark:shadow-md"
           >
             Explore Our Products
           </a>
